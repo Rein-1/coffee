3fa8f71c03434d21cbd74bf1ab23c6f4a0018908
@Rein-1 <markreniermercado123@gmail.com>
2024-11-17 23:33:55 +0800

    Updated Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Hero from "./components/Hero/Hero";
+import Hero from "./components/hero/Hero";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <main>
         <Hero />
       </main>
-    </div>
+    </>
   );
 }
